48fbbcee090ce15529812843062ee0af02d4b6ba
JasonHammer95 <jasonhammer1@mail.weber.edu>
2022-11-29 18:52:02 -0700

    Cut vehicle descriptions and left functions

--- Vehicle.py
+++ Vehicle.py
@@ -1,22 +1,17 @@
 class Vehicle:
+
+    #class level attribute
+    type = "ground"
+    propulsion = "battery"
     
     def __init__(self, name, color, num_wheels, speed): #each object has a self to identify it 
         self.name = name #take perameters and assign them a value
         self.color = color
         self.num_wheels = num_wheels
         self.speed = speed 
-        self.type = "ground" #sets all vehicles default type to "ground"
 
-
-bug_object = Vehicle("beetle", "yellow", 4, 1) #object(or instance) of vehicle class
-turtle = Vehicle("turtlebot", "green", 2, 5) #turtle is also an object, don't need to say object for it to be an object
-rover = Vehicle("rover", "purple", 4, 25) #third object
-drone = Vehicle("droney", "red", 0, 50)
-
-drone.type = "air"
-
-print(drone.type)
-
-print(bug_object) #can callout where object is in mempory, or callout for value of 
-print(turtle.color) #can callout value of an attribute
-print('This vehicle is', rover.color, 'with', rover.num_wheels, 'wheels, and can drive', rover.speed, 'mph.')
+    def print_details(self):
+        print(self.name, 'is', self.color, 'and is able to drive', self.speed, 'mph')
+    
+    def paint_vehicle(self, newcolor):
+        self.color = newcolor
